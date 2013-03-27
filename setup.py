3050a5e14febface993d9c0580d2c93c3b733ee2
Úlfur Kristjánsson <ulfur@theawesometastic.com>
2013-03-27 20:25:18 +0000

    Correct version number is 0.20 rather than 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name = "django-thjodskra",
-    version = "0.2",
+    version = "0.20",
     description = "django-thjodskra is a simple app for integrating the icelandic national registry into your django project",
     author = "Úlfur Kristjánsson",
     author_email = "ulfur@theawesometastic.com",

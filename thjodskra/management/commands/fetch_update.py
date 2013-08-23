93fdb4820190abd24477ca5c90faa3ae669e974b
Úlfur Kristjánsson <ulfur@theawesometastic.com>
2013-08-23 14:48:01 +0000

    Fixed fetch_update command. FTP should be active not passive

--- thjodskra/management/commands/fetch_update.py
+++ thjodskra/management/commands/fetch_update.py
@@ -1,28 +1,40 @@
 #encoding: utf-8
-import sys
+import os, sys
+
 from datetime import datetime
 from ftplib import FTP
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from ...models import Person
-
+	
 class Command(BaseCommand):
 
-    help = u'Fetches update file'
-
-    def handle(self, *args, **options):
+	help = u'Fetches update file'
+	settings = settings.THJODSKRA
+	
+	def handle(self, *args, **options):
 		
-		fname = args[0] if len(args)>0 else 'thjodskra_%s.txt'%datetime.now().strftime('%d%m%Y')
-		outfile = open( fname, 'wb' )
+		outfile = self.get_file( args[0] if len(args)>0 else None )
+		ftp = self.get_ftp_connection()
 		
-		thjodskra = settings.THJODSKRA
-		ftp = FTP( thjodskra['host'] )
-		if thjodskra.has_key('username') and thjodskra.has_key('password'):
-			ftp.login( thjodskra['username'], thjodskra['password'] )
-		ftp.retrbinary( thjodskra['file'], outfile.write )
+		ftp.retrbinary( 'RETR %s'%self.settings['file'], outfile.write )
 		ftp.close()
 		
 		outfile.truncate()
-		outfile.close()
 		
+		fname = os.path.abspath( outfile.name )
 		sys.stdout.write( fname )
+		outfile.close()
+
+	def get_file( self, fname ):
+		if fname is None:
+			fname = self.settings.get( 'outfile', 'thjodskra_%s.txt' )%datetime.now().strftime('%d%m%Y')
+		return open( fname, 'wb' )
+		
+	def get_ftp_connection( self ):
+		ftp = FTP( )
+		ftp.set_pasv( False )
+		ftp.connect( self.settings['host'] )
+		if self.settings.has_key('username') and self.settings.has_key('password'):
+			ftp.login( self.settings['username'], self.settings['password'] )
+		return ftp
